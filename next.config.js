164ccad9c9eef8283990d455ed4661aad6db6b44
Kundan Bhosale <62865281+kundanbhosale@users.noreply.github.com>
2022-10-08 15:49:50 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,21 @@
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
+
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
-module.exports = withBundleAnalyzer({
+module.exports = (phase, { defaultConfig }) => {
+    if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      reactStrictMode: true
+    }
+  }
+  return withBundleAnalyzer({
   reactStrictMode: true,
-  // images: {
-  //   loader: 'akamai',
-  //   path: '/',
-  // },
-  // basePath: '/portfolio',
-  // assetPrefix: '/portfolio',
+  images: {
+     loader: 'akamai',
+     path: '/',
+   },
+   basePath: '/portfolio',
+   assetPrefix: '/portfolio',
 })
+}
