fbd5667b51733b48a0271be9f4b936c164551e31
Kundan Bhosale <kundanbhosale99@gmail.com>
2022-10-08 16:43:38 +0530

    updated language

--- components/UI/IconCards.tsx
+++ components/UI/IconCards.tsx
@@ -13,107 +13,107 @@ const IconCards = () => {
       <CatlogWrapper
         colWidth={!mobile ? '75px' : '50px'}
         gap={!mobile ? '2rem' : '1rem'}
-        className='text-sm'
+        className="text-sm"
       >
         <div>
           <Image
-            src='/javascript.svg'
-            alt='javascrip-logo'
-            title='Javascript'
+            src="/javascript.svg"
+            alt="javascrip-logo"
+            title="Javascript"
             width={imgHW}
             height={imgHW}
           />
-          <p className='text-sm'>Javascript</p>
+          <p className="text-sm">Javascript</p>
         </div>
         <div>
           <Image
-            src='/typescript.svg'
-            alt='typescript-logo'
-            title='Typescript'
+            src="/typescript.svg"
+            alt="typescript-logo"
+            title="Typescript"
             width={imgHW}
             height={imgHW}
           />
-          <p className='text-sm'>Typescript</p>
+          <p className="text-sm">Typescript</p>
         </div>
         <div>
           <Image
-            src='/php.svg'
-            alt='php-logo'
-            title='PHP'
+            src="/golang.svg"
+            alt="golang-logo"
+            title="Golang"
             width={imgHW}
             height={imgHW}
           />
-          <p className='text-sm'>PHP</p>
+          <p className="text-sm">PHP</p>
         </div>
         <div>
           <Image
-            src='/nodejs.svg'
-            alt='nodejs-logo'
-            title='Node JS'
+            src="/nodejs.svg"
+            alt="nodejs-logo"
+            title="Node JS"
             width={imgHW}
             height={imgHW}
           />
-          <p className='text-sm'>Node JS</p>
+          <p className="text-sm">Node JS</p>
         </div>
         <div>
           <Image
-            src='/next.svg'
-            alt='next-logo'
-            title='Next JS'
+            src="/next.svg"
+            alt="next-logo"
+            title="Next JS"
             width={nextHW}
             height={imgHW}
           />
-          <p className='text-sm'>Next JS</p>
+          <p className="text-sm">Next JS</p>
         </div>
         <div>
           <Image
-            src='/react.svg'
-            alt='react-logo'
-            title='React'
+            src="/react.svg"
+            alt="react-logo"
+            title="React"
             width={imgHW}
             height={imgHW}
           />
-          <p className='text-sm'>React</p>
+          <p className="text-sm">React</p>
         </div>
         <div>
           <Image
-            src='/mongodb.svg'
-            alt='mongodb-logo'
-            title='Mongo DB'
+            src="/mongodb.svg"
+            alt="mongodb-logo"
+            title="Mongo DB"
             width={imgHW}
             height={imgHW}
           />
-          <p className='text-sm'>Mongo DB</p>
+          <p className="text-sm">Mongo DB</p>
         </div>
         <div>
           <Image
-            src='/postgres.svg'
-            alt='postgres-logo'
-            title='Postgres SQL'
+            src="/postgres.svg"
+            alt="postgres-logo"
+            title="Postgres SQL"
             width={imgHW}
             height={imgHW}
           />
-          <p className='text-sm'>Postgres SQL</p>
+          <p className="text-sm">Postgres SQL</p>
         </div>
         <div>
           <Image
-            src='/redis.svg'
-            alt='redis-logo'
-            title='Redis'
+            src="/redis.svg"
+            alt="redis-logo"
+            title="Redis"
             width={imgHW}
             height={imgHW}
           />
-          <p className='text-sm'>Redis</p>
+          <p className="text-sm">Redis</p>
         </div>
         <div>
           <Image
-            src='/graphql.svg'
-            alt='graphql-logo'
-            title='GraphQL'
+            src="/graphql.svg"
+            alt="graphql-logo"
+            title="GraphQL"
             width={imgHW}
             height={imgHW}
           />
-          <p className='text-sm'>GraphQL</p>
+          <p className="text-sm">GraphQL</p>
         </div>
       </CatlogWrapper>
     </Fragment>
